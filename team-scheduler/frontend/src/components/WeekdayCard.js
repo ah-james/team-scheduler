52b7dda1c27013b8d1a5f1b91bd82f9d10215682
ah-james <11ahj11@gmail.com>
2021-03-20 23:34:22 -0400

    Building Out Home/Weekday

--- team-scheduler/frontend/src/components/WeekdayCard.js
+++ team-scheduler/frontend/src/components/WeekdayCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-export default class WeekdayCard extends React.Component {
+class WeekdayCard extends React.Component {
     render() {
+        // debugger
         return(
             <div>
                 <h1>{this.props.weekday}</h1>
+                {this.props.titles.map(title => <label>{title.attributes.title}<br/></label>)}
             </div>
         )
     }
 }
+
+export default WeekdayCard
--- team-scheduler/frontend/src/containers/HomeContainer.js
+++ team-scheduler/frontend/src/containers/HomeContainer.js
@@ -1,18 +1,42 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchTitles } from '../actions/titlesActions'
 import WeekdayCard from '../components/WeekdayCard'
 
-export default class HomeContainer extends React.Component {
+class HomeContainer extends React.Component {
+
+    componentDidMount() {
+        // this.props.fetchEmployees()
+        this.props.fetchTitles()
+    }
     render(){
+        // debugger
         return(
             <div>
-                <WeekdayCard weekday={'Monday'} />
-                <WeekdayCard weekday={'Tuesday'} />
-                <WeekdayCard weekday={'Wednesday'} />
-                <WeekdayCard weekday={'Thursday'} />
-                <WeekdayCard weekday={'Friday'} />
-                <WeekdayCard weekday={'Saturday'} />
-                <WeekdayCard weekday={'Sunday'} />
+                {/* <h1>Home Container</h1> */}
+                <WeekdayCard weekday={'Monday'} titles={this.props.titles}/>
+                <WeekdayCard weekday={'Tuesday'} titles={this.props.titles}/>
+                <WeekdayCard weekday={'Wednesday'} titles={this.props.titles}/>
+                <WeekdayCard weekday={'Thursday'} titles={this.props.titles}/>
+                <WeekdayCard weekday={'Friday'} titles={this.props.titles}/>
+                <WeekdayCard weekday={'Saturday'} titles={this.props.titles}/>
+                <WeekdayCard weekday={'Sunday'} titles={this.props.titles}/>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return { 
+        // employees: state.employees,
+        titles: state.titles
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchTitles: () => dispatch(fetchTitles()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer)
