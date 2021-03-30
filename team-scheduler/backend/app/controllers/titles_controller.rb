da8fbcf98f8232a7d695d8f8dd580ad5b44685b1
ah-james <11ahj11@gmail.com>
2021-03-30 14:17:42 -0400

    Adding Create and Destroy Title to Backend

--- team-scheduler/backend/app/controllers/titles_controller.rb
+++ team-scheduler/backend/app/controllers/titles_controller.rb
@@ -3,4 +3,19 @@ class TitlesController < ApplicationController
         titles = Title.all
         render json: TitleSerializer.new(titles)
     end
+
+    def create
+        title = Title.find_by(id: params[:id])
+        if title.save
+            render json: TitleSerializer.new(title)
+        else
+            render json: { error: "title didn't save"}
+        end
+    end
+
+    def destroy
+        title = Title.find_by(id: params[:id])
+        title.destroy
+        render json: { error: "title didn't delete" }
+    end
 end
