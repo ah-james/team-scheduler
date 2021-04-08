e114f8683d6dac74661e93e8165ad29b6a34edb2
ah-james <11ahj11@gmail.com>
2021-04-07 20:36:53 -0400

    Changing EmployeeCard from Functional to Class

--- team-scheduler/frontend/src/components/EmployeeCard.js
+++ team-scheduler/frontend/src/components/EmployeeCard.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
-const EmployeeCard = (props) => {
-    return(
-        <div class="col-md-3">
-            <h2>{props.employee.attributes.name}</h2>
-            <img src={props.employee.attributes.image} alt="employee" />
-            <p>Experience: {props.employee.attributes.years} Years</p>
-            <p>Employee of the Month: {props.employee.attributes.awards}</p>
-            <button name="delete" onClick={() => props.delete(props.employee.id)}>Fire</button>
-            <button name="edit" onClick={() => props.handleEdit(props.employee)}>Edit</button>
-            <button name='award' onClick={() => props.addAward()}>Award</button>
-        </div>
-    )
+class EmployeeCard extends React.Component {
+
+    render() { 
+        return(
+            <div class="col-md-3">
+                <h2>{props.employee.attributes.name}</h2>
+                <img src={props.employee.attributes.image} alt="employee" />
+                <p>Experience: {props.employee.attributes.years} Years</p>
+                <p>Employee of the Month: {props.employee.attributes.awards}</p>
+                <button name="delete" onClick={() => props.delete(props.employee.id)}>Fire</button>
+                <button name="edit" onClick={() => props.handleEdit(props.employee)}>Edit</button>
+                <button name='award' onClick={() => props.addAward(props.employee)}>Award</button>
+            </div>
+        )
+    }
 }
 
 export default EmployeeCard
