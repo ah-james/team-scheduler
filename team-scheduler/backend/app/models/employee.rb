a005b8bc18e04e987c6d3be39525eb0f4332102e
ah-james <11ahj11@gmail.com>
2021-04-09 18:47:53 -0400

    Starting Backend Default Values

--- team-scheduler/backend/app/models/employee.rb
+++ team-scheduler/backend/app/models/employee.rb
@@ -1,3 +1,13 @@
 class Employee < ApplicationRecord
     belongs_to :title
+
+    after_initialize :default_values
+
+    private
+
+    def default_values
+        self.name = "Unknown Employee" if self.name.nil?
+        self.image = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png" if self.image.nil?
+        self.title_id = 2 if self.title_id.nil?
+    end
 end
