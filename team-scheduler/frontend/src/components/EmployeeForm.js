d3c248a6dfaa38245d5fe888b03b33258a2f702d
ah-james <11ahj11@gmail.com>
2021-04-12 11:30:16 -0400

    Starting Frontend Validations -- validateForm method

--- team-scheduler/frontend/src/components/EmployeeForm.js
+++ team-scheduler/frontend/src/components/EmployeeForm.js
@@ -9,6 +9,32 @@ class EmployeeForm extends React.Component {
         years: 0,
         awards: 0,
         title_id: '',
+        errors: {}
+    }
+
+    validateForm = () => {
+        let errors = {}
+        let formIsValid = true
+
+        if (!this.state.name) {
+            formIsValid = false
+            errors['name'] = '*Please enter a name'
+        }
+
+        if (!this.state.image) {
+            formIsValid = false
+            errors['name'] = '*Please enter an image'
+        }
+
+        if (this.state.name) {
+            if (!this.state.name.match(/^\w+$/)) {
+                formIsValid = false
+                errors['name'] = '*Please use alphabetic characters only'
+            }
+        }
+
+        this.setState({ errors })
+        return formIsValid
     }
 
     handleChange = (event) => {
@@ -20,7 +46,9 @@ class EmployeeForm extends React.Component {
 
     handleSubmit = event => {
         event.preventDefault()
-        this.props.addEmployee(this.state)
+        if (this.validateForm()) {
+            this.props.addEmployee(this.state)
+        }
         this.setState({
             name: '',
             image: '',
@@ -38,9 +66,11 @@ class EmployeeForm extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <label>Name: </label>
                     <input type='text' value={this.state.name} onChange={this.handleChange} name="name"/>
+                    <div className='error'>{this.state.errors.name}</div>
                     <br /><br />
                     <label>Image: </label>
                     <input type='text' value={this.state.image} onChange={this.handleChange} name="image"/>
+                    <div className='error'>{this.state.errors.image}</div>
                     <br /><br />
                     <label>Years: </label>
                     <input type='number' value={parseInt(this.state.years)} onChange={this.handleChange} name="years"/>
