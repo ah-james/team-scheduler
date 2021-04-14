eeb0e04d0cb625b9b634c67d7e9c2780978f5a7c
ah-james <11ahj11@gmail.com>
2021-04-14 15:16:44 -0400

    Employee Select Filter Functional

--- team-scheduler/frontend/src/components/EmployeeSelect.js
+++ team-scheduler/frontend/src/components/EmployeeSelect.js
@@ -5,10 +5,10 @@ const EmployeeSelect = (props) => {
         <div>
             <select class={`${props.weekday}-selector`}>
                 <option id={`employee-0`}></option>
-                {props.employees.map(employee => <option id={`employee-${employee.id}`} >{employee.attributes.name}</option>)}
-                {props.employees.filter(employee => employee.attributes.title_id === parseInt(props.titleId).map(filteredEmployee => (
-                    <option id={`employee-${employee.id}`} >{employee.attributes.name}</option>
-                )))}
+                {/* {props.employees.map(employee => <option id={`employee-${employee.id}`} >{employee.attributes.name}</option>)} */}
+                {props.employees.filter(employee => employee.attributes.title_id === parseInt(props.titleId)).map(filteredEmployee => (
+                    <option id={`employee-${filteredEmployee.id}`} >{filteredEmployee.attributes.name}</option>
+                ))}
             </select>
         </div>
     )
