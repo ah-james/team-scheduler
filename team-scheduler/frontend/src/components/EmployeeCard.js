3affaef13a9a564be3f496d84d91adcabd5d2e30
ah-james <11ahj11@gmail.com>
2021-04-19 16:03:46 -0400

    Adding Confirmation To Employee Delete

--- team-scheduler/frontend/src/components/EmployeeCard.js
+++ team-scheduler/frontend/src/components/EmployeeCard.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { increaseAward } from '../actions/employeesActions'
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 class EmployeeCard extends React.Component {
 
     addAward = (employee) => {
         this.props.increaseAward(employee)
     }
+
+    submit = () => {
+        confirmAlert({
+          title: 'Confirm to delete',
+          message: 'Are you sure to do delete this employee?',
+          buttons: [
+            {
+              label: 'Yes',
+              onClick: () => this.props.delete(this.props.employee.id)
+            },
+            {
+              label: 'No',
+              onClick: () => alert('Employee has been saved')
+            }
+          ]
+        });
+      };
 
     render() { 
         return(
@@ -16,7 +35,7 @@ class EmployeeCard extends React.Component {
                 <p>Experience: {this.props.employee.attributes.years} Years</p>
                 <p>Employee of the Month: {this.props.employee.attributes.awards}</p>
                 <div class="btn-group">
-                    <button class="btn btn-outline-dark" name="delete" onClick={() => this.props.delete(this.props.employee.id)}>Fire</button>
+                    <button class="btn btn-outline-dark" name="delete" onClick={() => this.submit()}>Fire</button>
                     <button class="btn btn-outline-dark" onClick={() => this.props.handleEdit(this.props.employee)}>Edit</button>
                     <button class="btn btn-outline-dark" name='award' onClick={() => this.addAward(this.props.employee)}>Award</button>
                 </div>
