f623b7a1b6b8dd780f51918093b0076c5aa07b6d
ah-james <11ahj11@gmail.com>
2021-07-04 12:01:27 -0400

    Changing Employee Card from Class to Functional Component

--- team-scheduler/frontend/src/components/EmployeeCard.js
+++ team-scheduler/frontend/src/components/EmployeeCard.js
@@ -4,44 +4,42 @@ import { increaseAward } from '../actions/employeesActions'
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
-class EmployeeCard extends React.Component {
+const EmployeeCard = props => {
 
-    addAward = (employee) => {
-        this.props.increaseAward(employee)
-    }
+  const addAward = (employee) => {
+      this.props.increaseAward(employee)
+  }
 
-    submit = () => {
-        confirmAlert({
-          title: 'Confirm to delete',
-          message: 'Are you sure to do delete this employee?',
-          buttons: [
-            {
-              label: 'Yes',
-              onClick: () => this.props.delete(this.props.employee.id)
-            },
-            {
-              label: 'No',
-              onClick: () => alert('Employee has been saved')
-            }
-          ]
-        });
-      };
+  const handleDelete = () => {
+    confirmAlert({
+      title: 'Confirm to delete',
+      message: 'Are you sure to do delete this employee?',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => props.delete(props.employee.id)
+        },
+        {
+          label: 'No',
+          onClick: () => alert('Employee has been saved')
+        }
+      ]
+    });
+  };
 
-    render() { 
-        return(
-            <div class="col-md-3 border border-dark m-4 p-4">
-                <h2>{this.props.employee.attributes.name}</h2>
-                <img src={this.props.employee.attributes.image} alt="employee" />
-                <p>Experience: {this.props.employee.attributes.years} Years</p>
-                <p>Employee of the Month: {this.props.employee.attributes.awards}</p>
-                <div class="btn-group">
-                    <button class="btn btn-outline-dark" name="delete" onClick={() => this.submit()}>Fire</button>
-                    <button class="btn btn-outline-dark" onClick={() => this.props.handleEdit(this.props.employee)}>Edit</button>
-                    <button class="btn btn-outline-dark" name='award' onClick={() => this.addAward(this.props.employee)}>Award</button>
-                </div>
-            </div>
-        )
-    }
+  return(
+      <div class="col-md-3 border border-dark m-4 p-4">
+          <h2>{props.employee.attributes.name}</h2>
+          <img src={props.employee.attributes.image} alt="employee" />
+          <p>Experience: {props.employee.attributes.years} Years</p>
+          <p>Employee of the Month: {props.employee.attributes.awards}</p>
+          <div class="btn-group">
+              <button class="btn btn-outline-dark" name="delete" onClick={() => handleDelete()}>Fire</button>
+              <button class="btn btn-outline-dark" onClick={() => props.handleEdit(props.employee)}>Edit</button>
+              <button class="btn btn-outline-dark" name='award' onClick={() => addAward(props.employee)}>Award</button>
+          </div>
+      </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => {
