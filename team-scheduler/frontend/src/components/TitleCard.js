9b15b5ee8c6adaa7531a7ac03f07479b4649827c
ah-james <11ahj11@gmail.com>
2021-07-04 12:03:51 -0400

    Changing Title Card from Class to Functional Component

--- team-scheduler/frontend/src/components/TitleCard.js
+++ team-scheduler/frontend/src/components/TitleCard.js
@@ -3,38 +3,36 @@ import { Link } from 'react-router-dom'
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
-class TitleCard extends React.Component {
+const TitleCard = props => {
 
-    submit = () => {
-        confirmAlert({
-          title: 'Confirm to delete',
-          message: 'Are you sure to do delete this employee?',
-          buttons: [
-            {
-              label: 'Yes',
-              onClick: () => this.props.delete(this.props.title.id)
-            },
-            {
-              label: 'No',
-              onClick: () => alert('Title has been saved')
-            }
-          ]
-        });
-      };
+  const submit = () => {
+    confirmAlert({
+      title: 'Confirm to delete',
+      message: 'Are you sure to do delete this employee?',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => props.delete(props.title.id)
+        },
+        {
+          label: 'No',
+          onClick: () => alert('Title has been saved')
+        }
+      ]
+    });
+  };
 
-    render(){
-        return(
-            <div class="col-sm">
-                <Link key={this.props.title.id} to={`/titles/${this.props.title.id}`}>{this.props.title.attributes.title_name}</Link> 
-                <br />
-                <button class="btn btn-outline-dark" name="delete" onClick={() => this.submit()}>Delete</button>
-                {this.props.employees.filter(employee => employee.attributes.title_id === parseInt(this.props.title.id)).map(filteredEmployee => (
-                    <li>{filteredEmployee.attributes.name} - {filteredEmployee.attributes.years} Years</li>
-                ))}
-                <br />
-            </div>
-        )
-    }
+  return(
+    <div class="col-sm">
+        <Link key={props.title.id} to={`/titles/${props.title.id}`}>{props.title.attributes.title_name}</Link> 
+        <br />
+        <button class="btn btn-outline-dark" name="delete" onClick={() => submit()}>Delete</button>
+        {props.employees.filter(employee => employee.attributes.title_id === parseInt(props.title.id)).map(filteredEmployee => (
+          <li>{filteredEmployee.attributes.name} - {filteredEmployee.attributes.years} Years</li>
+        ))}
+        <br />
+    </div>
+  )
 }
 
 export default TitleCard
