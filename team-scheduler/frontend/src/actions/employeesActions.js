7c2aba90dc4cde87db97240aa4538f73c27c70f0
ah-james <11ahj11@gmail.com>
2021-07-12 13:14:57 -0400

    Async/Await addEmployee and deleteEmployee Actions

--- team-scheduler/frontend/src/actions/employeesActions.js
+++ team-scheduler/frontend/src/actions/employeesActions.js
@@ -24,41 +24,107 @@ export const fetchEmployees = () => {
     }
 }
 
+// export const addEmployee = employee => {
+//     return (dispatch) => {
+//         fetch('http://localhost:3000/employees', {
+//             method: 'POST',
+//             body: JSON.stringify(employee),
+//             headers: {'Content-Type': 'application/json'}
+//         })
+//         .then(resp => resp.json())
+//         .then(json => dispatch({type: "ADD_EMPLOYEE", payload: json['data']}))
+//     }
+// }
+
 export const addEmployee = employee => {
-    return (dispatch) => {
-        fetch('http://localhost:3000/employees', {
-            method: 'POST',
-            body: JSON.stringify(employee),
-            headers: {'Content-Type': 'application/json'}
-        })
-        .then(resp => resp.json())
-        .then(json => dispatch({type: "ADD_EMPLOYEE", payload: json['data']}))
+    return async dispatch => {
+        try {
+            let response = await fetch('http://localhost:3000/employees', {
+                method: 'POST',
+                body: JSON.stringify(employee),
+                headers: {'Content-Type': 'application/json'}
+            })
+
+            if (!response.ok) {
+                throw new Error('Something Went Wrong!')
+            }
+
+            let json = await response.json()
+            dispatch({type: "ADD_EMPLOYEE", payload: json['data']})
+        } catch (error) {
+            throw error
+        }
     }
 }
+
+// export const deleteEmployee = employee => {
+//     return (dispatch) => {
+//         fetch(`http://localhost:3000/employees/${employee}`, {
+//             method: "DELETE",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "application/json"
+//             }
+//         })
+//         .then(resp => resp.json())
+//         .then(json => dispatch({type: "DELETE_EMPLOYEE", payload: json}))
+//     }
+// }
 
 export const deleteEmployee = employee => {
-    return (dispatch) => {
-        fetch(`http://localhost:3000/employees/${employee}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
+    return async dispatch => {
+        try {
+            let response = await fetch(`http://localhost:3000/employees/${employee}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            })
+
+            if (!response.ok) {
+                throw new Error('Something Went Wrong')
             }
-        })
-        .then(resp => resp.json())
-        .then(json => dispatch({type: "DELETE_EMPLOYEE", payload: json}))
+
+            let json = await response.json()
+            dispatch({type: "DELETE_EMPLOYEE", payload: json})
+            
+        } catch (error) {
+            throw error
+        }
     }
 }
 
+// export const editEmployee = employee => {
+//     return (dispatch) => {
+//         fetch(`http://localhost:3000/employees/${employee.id}`, {
+//         method: 'PATCH',
+//         body: JSON.stringify(employee),
+//         headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
+//         })
+//         .then(resp => resp.json())
+//         .then(json => dispatch({type: 'EDIT_EMPLOYEE', payload: json['data']}))
+//     }
+// }
+
 export const editEmployee = employee => {
-    return (dispatch) => {
-        fetch(`http://localhost:3000/employees/${employee.id}`, {
-        method: 'PATCH',
-        body: JSON.stringify(employee),
-        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
-        })
-        .then(resp => resp.json())
-        .then(json => dispatch({type: 'EDIT_EMPLOYEE', payload: json['data']}))
+    return async dispatch => {
+        try {
+            let response = await fetch(`http://localhost:3000/employees/${employee.id}`, {
+                method: 'PATCH',
+                body: JSON.stringify(employee),
+                headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
+            })
+
+            if (!response.ok) {
+                throw new Error('Something Went Wrong!')
+            }
+
+            let json = await response.json()
+            dispatch({type: 'EDIT_EMPLOYEE', payload: json['data']})
+        } catch (error) {
+            throw error
+        }
     }
 }
 
