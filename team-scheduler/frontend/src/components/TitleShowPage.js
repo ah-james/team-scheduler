46f55f9e990fa33b7263692da12365d3a2758cdc
ah-james <11ahj11@gmail.com>
2021-07-13 13:17:37 -0400

    TitleShowPage Class to Functional Component

--- team-scheduler/frontend/src/components/TitleShowPage.js
+++ team-scheduler/frontend/src/components/TitleShowPage.js
@@ -2,22 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import EmployeeCard from './EmployeeCard'
 
-class TitleShowPage extends React.Component {
+const TitleShowPage = props => {
+    let titleDetails = props.titles.filter(title => title.id == props.match.params.id)[0]
 
-    render() {
-        let titleDetails = this.props.titles.filter( t => t.id === this.props.match.params.id)[0];
-        return(
-            <div>
-                <h2>{titleDetails.attributes.title_name} Employees</h2>
-                <h3>{titleDetails.attributes.description}</h3>
-                <div class="row">
-                    {this.props.employees.filter(employee => employee.attributes.title_id === parseInt(this.props.match.params.id)).map(filteredEmployee => (
+    return(
+        <div>
+            <h2>{titleDetails.attributes.title_name} Employees</h2>
+            <h3>{titleDetails.attributes.description}</h3>
+            <div class="row">
+                {props.employees.filter(employee => employee.attributes.title_id === parseInt(props.match.params.id)).map(filteredEmployee => (
                     <EmployeeCard employee={filteredEmployee} />
-                    ))}
-                </div>
+                ))}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 function mapStateToProps(state) {
