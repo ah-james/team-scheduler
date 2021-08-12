22878b96b35ce16cde1f48bace077b8adee8fee3
ah-james <11ahj11@gmail.com>
2021-08-12 19:36:17 -0400

    Adding react-hook-form Package

--- team-scheduler/frontend/src/components/EmployeeForm.js
+++ team-scheduler/frontend/src/components/EmployeeForm.js
@@ -10,33 +10,48 @@ const EmployeeForm = props => {
     const [years, setYears] = useState(0)
     const [awards, setAwards] = useState(0)
     const [title_id, setTitleId] = useState('')
-    const [errors, setErrors] = useState({})
+    const [formErrors, setFormErrors] = useState({
+        name: '',
+        image: ''
+    })
 
     const titles = useSelector(state => state.titles)
     const dispatch = useDispatch()
 
     const validateForm = () => {
-        let errors = {}
+        // let formErrors = {}
         let formIsValid = true
 
         if (!name) {
             formIsValid = false
-            errors['name'] = '*Please enter a name'
+            // formErrors['name'] = '*Please enter a name'
+            setFormErrors({ 
+                ...formErrors,
+                [name]: '*Please enter a name' 
+            })
         }
 
         if (!image) {
             formIsValid = false
-            errors['image'] = '*Please enter an image'
+            // formErrors['image'] = '*Please enter an image'
+            setFormErrors({ 
+                ...formErrors,
+                [image]: '*Please enter an image' 
+            })
         }
 
         if (name) {
             if (!name.match(/[a-zA-Z]/)) {
                 formIsValid = false
-                errors['name'] = '*Please only use letters'
+                // formErrors['name'] = '*Please only use letters'
+                setFormErrors({
+                    ...formErrors, 
+                    [name]: '*Please only use letters' 
+                })
             }
         }
 
-        setErrors({ errors })
+        // setErrors(formErrors)
         return formIsValid
     }
 
@@ -50,7 +65,7 @@ const EmployeeForm = props => {
         setYears(0)
         setAwards(0)
         setTitleId('')
-        setErrors({})
+        setFormErrors({})
     }   
 
     return(
@@ -61,13 +76,13 @@ const EmployeeForm = props => {
                 <div>
                     <label>Name</label>
                     <input type='text' value={name} onChange={e => setName(e.target.value)} name="name"/>
-                    <div class='text-danger'>{errors.name}</div>
+                    <div class='text-danger'>{formErrors.name}</div>
                 </div>
                 <br /><br />
                 <div>
                     <label>Image</label>
                     <input type='text' value={image} onChange={e => setImage(e.target.value)} name="image"/>
-                    <div class='text-danger'>{errors.image}</div>
+                    <div class='text-danger'>{formErrors.image}</div>
                 </div>
                 <br /><br />
                 <div>
--- team-scheduler/frontend/src/index.js
+++ team-scheduler/frontend/src/index.js
@@ -13,13 +13,11 @@ import {BrowserRouter} from 'react-router-dom'
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  // <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App /> 
-      </BrowserRouter>
-    </Provider>,
-  // </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
