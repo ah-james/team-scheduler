5a79295ec117f9907dd2e4989bf603b2c8cdd7dc
ah-james <11ahj11@gmail.com>
2021-09-23 14:17:08 -0400

    Created Search Field Component

---
+++ team-scheduler/frontend/src/components/Search.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Search = props => {
+    return(
+        <div>
+            <input placeholder='Search for an Employee' />
+        </div>
+    )
+}
+
+export default Search
