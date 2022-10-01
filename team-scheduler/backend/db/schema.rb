cdb7b985c04b28d2c68343e0c6dafa4efee6fc1e
ah-james <11ahj11@gmail.com>
2022-09-30 21:49:59 -0400

    Heroku backend connection

--- team-scheduler/backend/db/schema.rb
+++ team-scheduler/backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_10_205802) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "employees", force: :cascade do |t|
     t.integer "years"
     t.datetime "created_at", precision: 6, null: false
